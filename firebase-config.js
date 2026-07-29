c161aa467aa9235404d4ec03cadb15a3ffdf5a13
Todfreak08 <laniogsamantha08@gmail.com>
2026-07-29 15:12:23 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,14 @@
-// Firebase Configuration
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA5QX9AyP2_ktesCGvbAA4nQ2Y2zO7Szrs",
-  authDomain: "storage-monitoring-48413.firebaseapp.com",
-  databaseURL: "https://storage-monitoring-48413-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "storage-monitoring-48413",
-  storageBucket: "storage-monitoring-48413.firebasestorage.app",
-  messagingSenderId: "836795478981",
-  appId: "1:836795478981:web:a7ebcfa7676bb8a36ed1ac"
+    apiKey: "AIzaSyA5QX9AyP2_ktesCGvbAA4nQ2Y2zO7Szrs",
+    authDomain: "storage-monitoring-48413.firebaseapp.com",
+    databaseURL: "https://storage-monitoring-48413-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "storage-monitoring-48413",
+    storageBucket: "storage-monitoring-48413.firebasestorage.app",
+    messagingSenderId: "836795478981",
+    appId: "1:836795478981:web:a7ebcfa7676bb8a36ed1ac"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Realtime Database Reference
+const auth = firebase.auth();
 const database = firebase.database();
